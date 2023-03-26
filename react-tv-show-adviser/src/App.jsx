da598b209accbae522f7fe393ad32c0986f1778d
Zuhrdev <eliechardin.dev@gmail.com>
2023-03-26 23:03:12 +0200

    display tvshow background with api call

--- react-tv-show-adviser/src/App.jsx
+++ react-tv-show-adviser/src/App.jsx
@@ -1,4 +1,40 @@
 import "./global.css";
+import s from "./style.module.css";
+import { TVShowAPI } from "./api/tv-show";
+import { useEffect, useState } from "react";
+import { BACKDROP_BASE_URL } from "./config";
+
+
 export function App() {
-    return <div>Hello World</div>;
+    const [currentTVShow, setCurrentTVShow] = useState();
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if(populars.length > 0) {
+            setCurrentTVShow(populars[0]);
+        }
+    }
+
+    useEffect(()=>{
+        fetchPopulars();
+    }, [])
+
+    console.log("***", currentTVShow);
+    return ( 
+    <div className={s.main_container}
+    style={{background: currentTVShow 
+        ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` 
+        : "black"}}>
+        <div className={s.header}>
+            <div className="row">
+                <div className="col-4">
+                    <div>Logo</div>
+                    <div>Subtitile</div>
+                </div>
+                <div className="col-md-12 col-lg-4"><input type="text"/></div>
+            </div>
+        </div>
+        <div className={s.tv_show_detail}>Details</div>
+        <div className={s.recommandations}>Recommandations</div>
+    </div>
+    );
 }
---
+++ react-tv-show-adviser/src/config.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=98b2a3999e7e40f34d52b9c8bc04879d";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL };
